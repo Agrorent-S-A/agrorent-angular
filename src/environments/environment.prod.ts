14edd1a82e12ac04fd8e2727a5d2d7631cc1ca32
Álvaro Mena Parra <alvaromepa@gmail.com>
2022-06-08 12:16:49 +0200

    hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+   firebaseConfig : {
+    apiKey: "AIzaSyAFx7VGkl0D2CmbPk8t__kgpHSLNpkfdCQ",
+    authDomain: "login-agrorent.firebaseapp.com",
+    projectId: "login-agrorent",
+    storageBucket: "login-agrorent.appspot.com",
+    messagingSenderId: "278598815694",
+    appId: "1:278598815694:web:f080acc594f513f121e59f",
+    measurementId: "G-JJCGS9PPPQ"
+  }
 };
